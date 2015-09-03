84576022f99454fac01d2c09ca2d58f10f7aa1a7
yokesharun <ayokesh.cse@gmail.com>
2015-09-03 12:08:00 +0530

    feed issues fixed

--- core/parse_xml.php
+++ core/parse_xml.php
@@ -14,14 +14,15 @@
     $link   = (string)$item; // Url Link
     $first_url = (string) $item->FirstURL;
     $img = (string) $item->Icon; // Image
-    $rss_split[] = '<li class="collection-item avatar z-dept-4"> <img src="'.$img.'" alt="" class="circle">'.$link.' <p><a href="'.$first_url.'">'.$first_url.'</a></p></li>';    
+    $rss_split[] = '<li class="collection-item avatar z-dept-4"> <img src="'.$img.'" alt="" class="circle">'.$link.' <p><a href="'.$first_url.'">'.$first_url.'</a></p></li>';   
+    } 
     
     return $rss_split;
   }
 
-  function get_display($numrows) 
+  function get_display($numrows,$head) 
   {
-    $rss_split = $this->parse();
+     $rss_split = $this->parse();
     $i = 0;
     $rss_data = '';
     while ( $i < $numrows ) 
@@ -31,14 +32,9 @@
     }
     $trim = str_replace('', '',$this->feed);
     $user = str_replace('&lang=en-us&format=rss_200','',$trim);
-   
-    if(trim($rss_data) == "")
-    {
-       return false;
-    }else{
+
        return $rss_data;
-    }
-    // returning the whole data
+    
   }
 }
 ?>
